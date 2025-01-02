ca39595190073c0f673c8bc9fcebdbd73373edec
enrique <enriqueml@blogdeenrique.com>
2025-01-02 10:42:00 +0100

    Variables close #7

---
+++ variables/constants/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("Three hours in seconds are: {THREE_HOURS_IN_SECONDS}");
+}
---
+++ variables/shadowing/target/debug/deps/shadowing-6c52e5c508a0ee6c.d
@@ -0,0 +1,5 @@
+/home/enrique/proyectos_rust/rust-exercises/variables/shadowing/target/debug/deps/libshadowing-6c52e5c508a0ee6c.rmeta: src/main.rs
+
+/home/enrique/proyectos_rust/rust-exercises/variables/shadowing/target/debug/deps/shadowing-6c52e5c508a0ee6c.d: src/main.rs
+
+src/main.rs:
